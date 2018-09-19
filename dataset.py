83f6d7dfa3e8853ad927eae277c46e681ee8ca83
EricYang <soilwater0723@gmail.com>
2018-09-19 17:06:45 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,21 @@
+import os
+import math
+import numpy as np
+import pandas as nd
+from tqdm import tqdm
+
+class DataSet(object):
+    def __init__(self, size):
+        self.size= size
+
+    def train_data(self):
+        traindata=0
+        return traindata 
+    
+    def eval_data(self):
+        evaldata=0
+        return evaldata
+        
+    def test_data(self):
+        testdata=0
+        return testdata
